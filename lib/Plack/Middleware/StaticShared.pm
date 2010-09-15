9244fa88108abdd52bb0d1e63b9a8337b2a9c430
cho45 <cho45@lowreal.net>
2010-09-15 22:35:41 +0900

    license and .gitignore

--- lib/Plack/Middleware/StaticShared.pm
+++ lib/Plack/Middleware/StaticShared.pm
@@ -126,5 +126,10 @@ cho45
 
 L<Plack::Middleware> L<Plack::Builder>
 
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
 =cut
 
