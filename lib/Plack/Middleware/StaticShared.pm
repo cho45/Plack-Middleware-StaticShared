dcc9f80725699fad40be3e5459a2c4404398fcbb
shiba_yu36 <shibayu36@gmail.com>
2013-01-24 19:16:01 +0900

    fix bug that happens when specified file does not exist

--- lib/Plack/Middleware/StaticShared.pm
+++ lib/Plack/Middleware/StaticShared.pm
@@ -77,14 +77,19 @@ sub call {
 sub concat {
 	my ($self, @files) = @_;
 	my $base = dir($self->base);
-	join("",
-		map {
-			my $file = $base->file($_);
-			$file->resolve;
-			$base->contains($file) ? $file->slurp : '';
-		}
-		@files
-	);
+
+	my $concat = '';
+	for my $f (@files) {
+		my $file = $base->file($f);
+		next unless -e $file;
+
+		$file->resolve;
+		next unless $base->contains($file);
+
+		$concat .= $file->slurp;
+	}
+
+	return $concat;
 }
 
 1;
